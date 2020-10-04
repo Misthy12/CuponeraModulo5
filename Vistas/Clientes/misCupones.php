74459ef2a3a35c4b8f35d382b3492351fad3ff42
nancycmartinez <a_dramartinez@hotmail.com>
2020-10-04 01:04:56 -0600

    Cambios en mis cupones selecionar por id

--- Vistas/Clientes/misCupones.php
+++ Vistas/Clientes/misCupones.php
@@ -22,18 +22,36 @@
                     <?php
                             $conn =OpenCon();
                             $hoy= date("Y-m-d");//fecha actual
-                            $sql="SELECT o.idCupon as id,o.tituloOferta as titulo, s.nombreSucursal as sucursal, o.precioRegular, o.precioOferta, e.definirEstado as estado, o.descripcion, o.fechaInicio, o.fechaFin, o.fechaLimite  FROM tblCupones o
-                            INNER JOIN tblSucursales s ON o.idSucursal = s.idSucursal
-                            INNER JOIN tblEstadosCupon e ON o.estado=e.idEstadoCupon WHERE o.estado=2";
+                            $idClien=$_SESSION["id"];
+                            $sql="SELECT cp.idCompra as id, cp.codigoCompra, cp.idCupon, cl.nombresCliente, cl.apellidosClientes, o.idCupon, o.idSucursal,o.descripcion, o.precioRegular, o.precioOferta, o.descripcion, o.fechaInicio, o.fechaFin, o.fechaLimite FROM tblCompraCupon cp
+                            INNER JOIN tblcupones o ON cp.idCupon = o.idCupon
+                            INNER JOIN tblClientes cl ON cp.idCliente = cl.idCliente WHERE cp.idCliente=$idClien AND o.estado=2";
+
                             echo "<div class='row col-12'>";
                             //Imprecion de formulario
                             foreach($conn->query($sql) as $row){
+                                $sucursal=$row["idSucursal"];
+                                $sqlSucursal=$conn->prepare("SELECT nombreSucursal FROM tblSucursales WHERE idSucursal= $sucursal ");
+                                $sqlSucursal->execute(array($sucursal));
+                                $count=$sqlSucursal->rowCount();
+                                $rowS=$sqlSucursal->fetchAll(PDO::FETCH_OBJ);
+                                if($count==0)
+                                {
+                                    ECHO "<h1 class='text-center text-warning'>NO DISPONE DE CUPONES!!</h1>";
+                                }
+                                else{
+                                foreach($rowS as $rowS){}
                                 echo "<div class='card col-sm-12 col-md-3' >";
-                                echo "<div class='card-header bg-success'> <h4 class='text-center'>OFERTA ACTIVA</h4></div>";
+                                echo "<div class='card-header bg-success'> 
+                                <h4 class='text-center'>OFERTA ACTIVA</h4>
+                                <h6 class='text-center'><b>Codigo de compra: </b>".$row["codigoCompra"]."</h6>
+                                </div>";
                                 // echo "<div class='card-body'>".$row["fechaInicio"].$row["fechaFin"]."</div>";
                                     echo "<div class='card-body '>";
-                                    echo "<h6 class='h5 text-center font-weight-bold'>".$row["sucursal"]."</h6>";
+                                    echo "<h6 class='h5 text-center font-weight-bold'>".$rowS->nombreSucursal."</h6>";
+                                    echo "<h6 class='h6 text-center font-weight-bold'> <b>Comprador: </b>".$row["nombresCliente"]." ".$row["apellidosClientes"] ."</h6>";
                                     echo "<p class='text-lg-justify'> <b>OFERTA! </b>".$row["descripcion"]."</p> <hr>";
+                                    echo "<p class='text-lg-center'> <b>Precio Oferta: </b>$".$row["precioOferta"]."</p> <hr>";
                                     echo "<p class='text-lg-center'> <b>Precio Oferta: </b>$".$row["precioOferta"]."</p> <hr>";
                                     echo "<p class='text-lg-center'> <b>Precio Regular: </b><s class='text-danger'>$".$row["precioRegular"]."</s></p>";
                                     echo "</div>";
@@ -41,6 +59,7 @@
                                     echo "</div></div>";
                                     // }
                                 }
+                            }
                             echo "</div>";
                             CloseCon($conn);
                     ?>
@@ -50,14 +69,24 @@
                 <div class="card card-body">
                     <?php
                             $conn =OpenCon();
-                            $hoy= date("Y-m-d");//fecha actual
-                            $sql="SELECT o.idCupon as id,o.tituloOferta as titulo, s.nombreSucursal as sucursal, o.precioRegular, o.precioOferta, e.definirEstado as estado, o.descripcion, o.fechaInicio, o.fechaFin, o.fechaLimite  FROM tblCupones o
-                            INNER JOIN tblSucursales s ON o.idSucursal = s.idSucursal
-                            INNER JOIN tblEstadosCupon e ON o.estado=e.idEstadoCupon WHERE o.estado=5 ";
+                            $sql="SELECT cp.idCompra as id, cp.codigoCompra, cp.idCupon, cl.nombresCliente, cl.apellidosClientes, o.idCupon, o.idSucursal,o.descripcion, o.precioRegular, o.precioOferta, o.descripcion, o.fechaInicio, o.fechaFin, o.fechaLimite FROM tblCompraCupon cp
+                            INNER JOIN tblcupones o ON cp.idCupon = o.idCupon
+                            INNER JOIN tblClientes cl ON cp.idCliente = cl.idCliente WHERE cp.idCliente=$idClien AND o.estado=5";
 
-                            //Imprecion de formulario
                             echo "<div class='row col-12'>";
+                            //Imprecion de formulario
                             foreach($conn->query($sql) as $row){
+                                $sucursal=$row["idSucursal"];
+                                $sqlSucursal=$conn->prepare("SELECT nombreSucursal FROM tblSucursales WHERE idSucursal= $sucursal ");
+                                $sqlSucursal->execute(array($sucursal));
+                                $count=$sqlSucursal->rowCount();
+                                $rowS=$sqlSucursal->fetchAll(PDO::FETCH_OBJ);
+                                if($count==0)
+                                {
+                                    ECHO "<h1 class='text-center text-warning'>NO DISPONE DE CUPONES!!</h1>";
+                                }
+                                else{
+                                foreach($rowS as $rowS){}
                                 echo "<div class='card col-sm-12 col-md-3'>";
                                 echo "<div class='card-header bg-danger'> <h4 class='text-center'>OFERTA VENCIDA</h4></div>";
                                 echo "<div class='card-body'>";
@@ -70,6 +99,7 @@
                                 echo "</div></div>";
 
                                 }
+                            }
                                 echo "</div>";
                                 CloseCon($conn);
                     ?>
